5771c15f04b9992613278da0f9efa3a49bc5710c
mccken <747807013@qq.com>
2019-08-09 09:35:24 +0800

    druid

--- order/src/main/java/com/mccken201908/order/OrderApplication.java
+++ order/src/main/java/com/mccken201908/order/OrderApplication.java
@@ -1,5 +1,6 @@
 package com.mccken201908.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -7,6 +8,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan(basePackages = "com.mccken201908.order.dao")
 public class OrderApplication {
 
 	public static void main(String[] args) {
--- order/src/main/java/com/mccken201908/order/controller/OrderController.java
+++ order/src/main/java/com/mccken201908/order/controller/OrderController.java
@@ -1,8 +1,13 @@
 package com.mccken201908.order.controller;
 
+import com.mccken201908.order.model.McckenOrder;
+import com.mccken201908.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
@@ -10,9 +15,18 @@ public class OrderController {
 	@Value("${server.port}")
 	private String port;
 	
+	@Autowired
+	private OrderService orderService;
+	
 	@GetMapping("/show-order")
 	public String showOrder(String orderId) {
 		
 		return "show port: " + port + ", orderId is: " + orderId;
 	} 
+	
+	@GetMapping("/hi-order")
+	public List<McckenOrder> selectAllOrder() {
+		
+		return this.orderService.selectAllOrder();
+	}
 }
